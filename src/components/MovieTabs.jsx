5d5df33094e4efd2193f0d385be9b5a99ab28e2c
Alexander Petriga <cazhqa666@gmail.com>
2020-11-16 14:10:41 +0300

    refector MovieTabs

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -3,34 +3,38 @@ import React from 'react';
 const MovieTabs = (props) => {
   const { sort_by, updateSortBy } = props;
 
+  const handleClick = value => {
+    return () => {
+      updateSortBy(value)
+    }
+  }
+
+  const getClassActive = value => {
+    return `nav-link ${sort_by === value ? "active" : ""}`
+  }
+
   return (
     <ul className="tabs nav nav-pills">
       <li className="nav-item">
         <div
-          className={`nav-link ${sort_by === "popularity.desc" ? "active" : ""}`}
-          onClick={() => {
-            updateSortBy("popularity.desc")
-          }}
+          className={getClassActive("popularity.desc")}
+          onClick={handleClick("popularity.desc")}
         >
           Popularity desc
         </div>
       </li>
       <li className="nav-item">
         <div
-          className={`nav-link ${sort_by === "revenue.desc" ? "active" : ""}`}
-          onClick={() => {
-            updateSortBy("revenue.desc")
-          }}
+          className={getClassActive("revenue.desc")}
+          onClick={handleClick("revenue.desc")}
         >
           Revenue desc
         </div>
       </li>
       <li className="nav-item">
         <div
-          className={`nav-link ${sort_by === "vote_average.desc" ? "active" : ""}`}
-          onClick={() => {
-            updateSortBy("vote_average.desc")
-          }}
+          className={getClassActive("vote_average.desc")}
+          onClick={handleClick("vote_average.desc")}
         >
           Vote Average desc
         </div>
